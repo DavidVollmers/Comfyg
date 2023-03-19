cc016fca6c44aea8a88fed56aef5ef33e7f8783d
dvolper <github@david.vollmers.org>
2023-03-19 11:02:04 +0100

    local dockerfile build from CLI

--- packages/Comfyg.Cli/Commands/Setup/SetupLocalhostCommand.cs
+++ packages/Comfyg.Cli/Commands/Setup/SetupLocalhostCommand.cs
@@ -1,13 +1,17 @@
 ﻿using System.CommandLine;
 using System.CommandLine.Invocation;
+using System.Text;
 using Docker.DotNet;
 using Docker.DotNet.Models;
+using ICSharpCode.SharpZipLib.Tar;
 using Spectre.Console;
 
 namespace Comfyg.Cli.Commands.Setup;
 
 public class SetupLocalhostCommand : Command
 {
+    private const string DockerImageLocalBuildTag = "comfyg-local-build";
+    
     private readonly Option<FileInfo?> _dockerFileOption;
     private readonly Option<Uri?> _dockerSocketOption;
 
@@ -18,12 +22,14 @@ public class SetupLocalhostCommand : Command
             "-df",
             "--docker-file"
         }, "The Dockerfile to use for building the Comfyg API");
+        AddOption(_dockerFileOption);
 
         _dockerSocketOption = new Option<Uri?>(new[]
         {
             "-ds",
             "--docker-socket"
         }, "The URI to the docker socket to use");
+        AddOption(_dockerSocketOption);
 
         this.SetHandler(HandleCommandAsync);
     }
@@ -46,25 +52,18 @@ public class SetupLocalhostCommand : Command
             if (!dockerFileOption.Exists)
                 throw new FileNotFoundException("Could not find Dockerfile.", dockerFileOption.FullName);
 
-            var progress = new Markup("Starting Dockerfile build...");
+            AnsiConsole.WriteLine("Building Docker Image...");
+            var progress = new Text("Starting Dockerfile build...");
             await AnsiConsole.Live(progress).StartAsync(async displayContext =>
             {
-                void MessageHandler(object sender, JSONMessage message)
+                void MessageHandler(string message)
                 {
-                    if (message.ErrorMessage != null)
-                    {
-                        displayContext.UpdateTarget(new Markup($"[bold red]{message.ErrorMessage}[/]"));
-                    }
-                    else
-                    {
-                        displayContext.UpdateTarget(new Markup(message.ProgressMessage));
-                    }
-
+                    displayContext.UpdateTarget(new Text(message));
                     displayContext.Refresh();
                 }
 
                 // ReSharper disable once AccessToDisposedClosure
-                await BuildDockerfileAsync(dockerClient, dockerFileOption, MessageHandler!, cancellationToken)
+                await BuildDockerfileAsync(dockerClient, dockerFileOption, MessageHandler, cancellationToken)
                     .ConfigureAwait(false);
             }).ConfigureAwait(false);
         }
@@ -75,18 +74,69 @@ public class SetupLocalhostCommand : Command
         }
     }
 
-    private static async Task BuildDockerfileAsync(IDockerClient dockerClient, FileSystemInfo dockerFile,
-        EventHandler<JSONMessage> messageHandler, CancellationToken cancellationToken)
+    private static async Task BuildDockerfileAsync(IDockerClient dockerClient, FileInfo dockerFile,
+        Action<string> messageHandler, CancellationToken cancellationToken)
     {
-        using var stream = new MemoryStream();
+        await using var stream =
+            await CreateStreamFromDockerfileDirectoryAsync(dockerFile.Directory!, messageHandler, cancellationToken)
+                .ConfigureAwait(false);
 
         var progress = new Progress<JSONMessage>();
-        progress.ProgressChanged += messageHandler;
+        progress.ProgressChanged += (_, message) =>
+        {
+            if (message.Stream != null) messageHandler(message.Stream);
+        };
 
         await dockerClient.Images.BuildImageFromDockerfileAsync(new ImageBuildParameters
             {
-                Dockerfile = dockerFile.FullName
+                Dockerfile = dockerFile.Name,
+                Tags = new List<string>
+                {
+                    DockerImageLocalBuildTag
+                }
             }, stream, Array.Empty<AuthConfig>(), new Dictionary<string, string>(), progress, cancellationToken)
             .ConfigureAwait(false);
+    }
+
+    private static async Task<Stream> CreateStreamFromDockerfileDirectoryAsync(FileSystemInfo directoryInfo,
+        Action<string> messageHandler, CancellationToken cancellationToken)
+    {
+        messageHandler("Creating dockerfile build context...");
+
+        var stream = new MemoryStream();
+
+        await using var archive = new TarOutputStream(stream, Encoding.UTF8)
+        {
+            IsStreamOwner = false
+        };
+
+        var files = Directory.GetFiles(directoryInfo.FullName, "*.*", SearchOption.AllDirectories);
+        messageHandler($"Found {files.Length} files");
+
+        foreach (var file in files)
+        {
+            var name = file[directoryInfo.FullName.Length..].Replace('\\', '/').TrimStart('/');
+
+            messageHandler($"Adding \"{name}\" to the build context...");
+
+            var entry = TarEntry.CreateTarEntry(name);
+            await using var fileStream = File.OpenRead(file);
+            entry.Size = fileStream.Length;
+            await archive.PutNextEntryAsync(entry, cancellationToken);
+
+            int numRead;
+            var buffer = new byte[32 * 1024];
+            while ((numRead = await fileStream.ReadAsync(buffer, cancellationToken)) > 0)
+            {
+                await archive.WriteAsync(buffer.AsMemory(0, numRead), cancellationToken);
+            }
+
+            await archive.CloseEntryAsync(cancellationToken);
+        }
+
+        archive.Close();
+
+        stream.Position = 0;
+        return stream;
     }
 }
--- packages/Comfyg.Cli/README.md
+++ packages/Comfyg.Cli/README.md
@@ -14,4 +14,6 @@ dotnet pack
 dotnet tool install --add-source ./bin/Debug Comfyg.Cli --prerelease
 ```
 
+> You must commit and push your changes before this will update your local dotnet tool.
+
 You can read more about hot to use local .NET tools [here](https://learn.microsoft.com/en-us/dotnet/core/tools/local-tools-how-to-use).
