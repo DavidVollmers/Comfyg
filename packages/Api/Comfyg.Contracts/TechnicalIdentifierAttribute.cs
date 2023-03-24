407f75beacee8ad6a900750494a0c3aae91f38ac
dvolper <github@david.vollmers.org>
2023-03-24 09:54:46 +0100

    pre commit hook using husky

--- packages/Api/Comfyg.Contracts/TechnicalIdentifierAttribute.cs
+++ packages/Api/Comfyg.Contracts/TechnicalIdentifierAttribute.cs
@@ -7,12 +7,12 @@ internal class TechnicalIdentifierAttribute : ValidationAttribute
 {
     // https://learn.microsoft.com/en-us/rest/api/storageservices/Understanding-the-Table-Service-Data-Model#characters-disallowed-in-key-fields
     private static readonly byte[] DisallowedCharacters =
-        "/\\#?\t\n\r"u8.ToArray().Concat(new byte[32].Select((_, i) => (byte) (127 + i))).ToArray();
+        "/\\#?\t\n\r"u8.ToArray().Concat(new byte[32].Select((_, i) => (byte)(127 + i))).ToArray();
 
     public override bool IsValid(object? value)
     {
         if (value == null) return false;
-        
+
         if (value is not string s) throw new ValidationException("Only strings can be used as technical identifier.");
 
         return Encoding.UTF8.GetBytes(s).All(b => !DisallowedCharacters.Contains(b));
