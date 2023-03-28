319b354746a1ca7af9318f449ff994332133cc01
dvolper <github@david.vollmers.org>
2023-03-28 11:12:55 +0200

    code format

--- tests/Comfyg.Tests.Common/Contracts/TestConfigurationValue.cs
+++ tests/Comfyg.Tests.Common/Contracts/TestConfigurationValue.cs
@@ -5,12 +5,12 @@ namespace Comfyg.Tests.Common.Contracts;
 public class TestConfigurationValue : IConfigurationValue
 {
     public string Key { get; set; } = null!;
-    
+
     public string Value { get; set; } = null!;
-    
+
     public string Version { get; set; } = null!;
-    
+
     public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
-    
+
     public string Hash { get; set; } = null!;
 }
