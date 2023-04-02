706ce4b26b60b3f28a4184c83336f524b7e1d8a3
dvolper <github@david.vollmers.org>
2023-04-02 10:13:04 +0200

    value operations

--- examples/comfyg-examples-js/src/index.js
+++ examples/comfyg-examples-js/src/index.js
@@ -2,4 +2,7 @@ import ComfygClient from '@comfyg/client'
 
 const client = new ComfygClient(process.env.ComfygConnectionString)
 
-await client.configuration.addValues([{ key: 'Example', value: 'example value' }])
+const values = await client.configuration.getValuesAsync()
+for await (const value of values) {
+  console.log(value)
+}
