208a8fd69b298ad7b9925dbdd0ff7c6e00f8bdc9
dvolper <github@david.vollmers.org>
2023-04-18 08:13:13 +0200

    node SDK

--- packages/comfyg-client/lib/index.ts
+++ packages/comfyg-client/lib/index.ts
@@ -1,8 +1,10 @@
 import { ComfygClient } from './comfyg-client.js'
+import ComfygConstants from './comfyg-constants.js'
 
 export { Client } from './client.js'
 export { ConnectionResponse } from './responses/connection-response.js'
 export { SetupClientResponse } from './responses/setup-client-response.js'
 export { ComfygValue } from './comfyg-value.js'
 export { Permissions } from './permissions.js'
+export { ComfygConstants }
 export default ComfygClient
--- packages/comfyg-client/lib/value-operations.ts
+++ packages/comfyg-client/lib/value-operations.ts
@@ -2,22 +2,28 @@ import { ComfygValue } from './comfyg-value.js'
 import { ComfygClient } from './comfyg-client.js'
 import { Response } from 'node-fetch'
 import ReadableStream = NodeJS.ReadableStream
-import { Permissions } from './permissions'
+import { Permissions } from './permissions.js'
+import ComfygConstants from './comfyg-constants.js'
 
 export class ValueOperations {
   public constructor(private readonly _client: ComfygClient, private readonly _operation: string) {}
 
-  public async getValues(since: Date | null = null, signal: AbortSignal | null = null): Promise<ComfygValue[]> {
-    const response = await this.getValuesResponse(since, signal)
+  public async getValues(
+    since: Date | null = null,
+    tags: string[] = [],
+    signal: AbortSignal | null = null,
+  ): Promise<ComfygValue[]> {
+    const response = await this.getValuesResponse(since, tags, signal)
     const json = await response.json()
     return <ComfygValue[]>json
   }
 
   public async *getValuesAsync(
     since: Date | null = null,
+    tags: string[] = [],
     signal: AbortSignal | null = null,
   ): AsyncGenerator<ComfygValue> {
-    const response = await this.getValuesResponse(since, signal)
+    const response = await this.getValuesResponse(since, tags, signal)
     if (response.body == null) return
     for await (const chunk of response.body) {
       yield* JSON.parse(chunk.toString())
@@ -26,15 +32,31 @@ export class ValueOperations {
 
   public async getValuesStream(
     since: Date | null = null,
+    tags: string[] = [],
     signal: AbortSignal | null = null,
   ): Promise<ReadableStream | null> {
-    const response = await this.getValuesResponse(since, signal)
+    const response = await this.getValuesResponse(since, tags, signal)
     return response.body
   }
 
-  private async getValuesResponse(since: Date | null = null, signal: AbortSignal | null = null): Promise<Response> {
+  private async getValuesResponse(
+    since: Date | null = null,
+    tags: string[] = [],
+    signal: AbortSignal | null = null,
+  ): Promise<Response> {
+    let uri = '/' + this._operation
+    const params = new URLSearchParams()
+    if (since) {
+      params.set('since', since.toISOString())
+    }
+    if (tags && tags.length) {
+      for (const tag of tags) {
+        params.append('tags', encodeURIComponent(tag))
+      }
+    }
+    if ([...params].length) uri += '?' + params.toString()
     const response = await this._client.__sendRequest(
-      '/' + this._operation,
+      uri,
       {
         method: 'GET',
       },
@@ -44,6 +66,28 @@ export class ValueOperations {
       throw new Error(`Invalid status code when trying to get values (${this._operation}): ` + response.status)
     }
     return response
+  }
+
+  public async getValue(
+    key: string,
+    version: string | null = null,
+    signal: AbortSignal | null = null,
+  ): Promise<ComfygValue> {
+    if (key == null) throw new Error('Value cannot be null. Parameter name: key')
+    let uri = '/' + this._operation + '/' + encodeURIComponent(key)
+    if (version) uri += '/' + encodeURIComponent(version)
+    const response = await this._client.__sendRequest(
+      uri,
+      {
+        method: 'GET',
+      },
+      signal,
+    )
+    if (!response.ok) {
+      throw new Error(`Invalid status code when trying to get value (${this._operation}): ` + response.status)
+    }
+    const json = await response.json()
+    return <ComfygValue>json
   }
 
   public async addValues(values: ComfygValue[], signal: AbortSignal | null = null): Promise<void> {
@@ -62,6 +106,33 @@ export class ValueOperations {
     if (!response.ok) {
       throw new Error(`Invalid status code when trying to add values (${this._operation}): ` + response.status)
     }
+  }
+
+  public async tagValue(
+    key: string,
+    tag: string,
+    version: string = ComfygConstants.LatestVersion,
+    signal: AbortSignal | null = null,
+  ): Promise<ComfygValue> {
+    if (key == null) throw new Error('Value cannot be null. Parameter name: key')
+    if (tag == null) throw new Error('Value cannot be null. Parameter name: tag')
+    if (version == null) throw new Error('Value cannot be null. Parameter name: version')
+    const response = await this._client.__sendRequest(
+      '/' + this._operation + '/tags',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ key, tag, version }),
+      },
+      signal,
+    )
+    if (!response.ok) {
+      throw new Error(`Invalid status code when trying to tag value (${this._operation}): ` + response.status)
+    }
+    const json = await response.json()
+    return <ComfygValue>json
   }
 
   public async setPermission(
